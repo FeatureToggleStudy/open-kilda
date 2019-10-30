81fd233fc61325c9d32a84c6a84cc339bc3cde3c
Andrii Dovhan <dovhan.andriy@gmail.com>
2019-10-30 16:53:01 +0200

    add test for kilda configuration

--- services/src/functional-tests/src/test/groovy/org/openkilda/functionaltests/spec/configuration/ConfigurationSpec.groovy
+++ services/src/functional-tests/src/test/groovy/org/openkilda/functionaltests/spec/configuration/ConfigurationSpec.groovy
@@ -1,11 +1,19 @@
 package org.openkilda.functionaltests.spec.configuration
 
+import static org.junit.Assume.assumeTrue
 import static org.openkilda.functionaltests.extension.tags.Tag.HARDWARE
 import static org.openkilda.functionaltests.extension.tags.Tag.LOW_PRIORITY
+import static org.openkilda.testing.Constants.WAIT_OFFSET
+import static org.openkilda.testing.Constants.WAIT_OFFSET
+import static org.openkilda.testing.Constants.WAIT_OFFSET
+import static org.openkilda.testing.Constants.WAIT_OFFSET
 
 import org.openkilda.functionaltests.HealthCheckSpecification
 import org.openkilda.functionaltests.extension.tags.Tags
+import org.openkilda.functionaltests.helpers.Wrappers
 import org.openkilda.messaging.error.MessageError
+import org.openkilda.messaging.info.event.IslChangeType
+import org.openkilda.messaging.info.event.SwitchChangeType
 import org.openkilda.messaging.model.system.KildaConfigurationDto
 import org.openkilda.model.FlowEncapsulationType
 
@@ -78,5 +86,69 @@ class ConfigurationSpec extends HealthCheckSpecification {
             northbound.updateKildaConfiguration(
                     new KildaConfigurationDto(flowEncapsulationType: defaultEncapsulationType))
         }
+    }
+
+    def "System takes into account default multi table value while connecting a new switch"() {
+        /*antiflapCooldown should be rather big in order for test to understand that there is no 'antiflap' during
+       isl discovery*/
+        assumeTrue(antiflapCooldown >= discoveryInterval + WAIT_OFFSET / 2)
+
+        setup: "Disconnect one of the switches and remove it from DB. Pretend this switch never existed"
+        def initConf = northbound.getKildaConfiguration()
+        def sw = topology.activeSwitches.first()
+        def isls = topology.getRelatedIsls(sw)
+        lockKeeper.knockoutSwitch(sw)
+        Wrappers.wait(discoveryTimeout + WAIT_OFFSET) {
+            assert northbound.getSwitch(sw.dpId).state == SwitchChangeType.DEACTIVATED
+            assert northbound.getAllLinks().findAll { it.state == IslChangeType.FAILED }.size() == isls.size() * 2
+        }
+        isls.each { northbound.deleteLink(islUtils.toLinkParameters(it)) }
+        northbound.deleteSwitch(sw.dpId, false)
+
+        and: "Update the multi table field in kilda configuration"
+        def newMultiTableValue = !initConf.useMultiTable
+        northbound.updateKildaConfiguration(northbound.getKildaConfiguration().tap {
+            it.useMultiTable = newMultiTableValue
+        })
+
+        when: "New switch connects"
+        lockKeeper.reviveSwitch(sw)
+        Wrappers.wait(discoveryInterval + WAIT_OFFSET) {
+            assert northbound.getSwitch(sw.dpId).state == SwitchChangeType.ACTIVATED
+            def allIsls = northbound.getAllLinks()
+            isls.each {
+                assert islUtils.getIslInfo(allIsls, it).get().state == IslChangeType.DISCOVERED
+                assert islUtils.getIslInfo(allIsls, it.reversed).get().state == IslChangeType.DISCOVERED
+            }
+        }
+
+        then: "Switch is added with switch property according to the kilda configuration"
+        northbound.getSwitchProperties(sw.dpId).multiTable == newMultiTableValue
+
+        when: "Delete the switch and remove it from DB"
+        lockKeeper.knockoutSwitch(sw)
+        Wrappers.wait(discoveryTimeout + WAIT_OFFSET) {
+            assert northbound.getSwitch(sw.dpId).state == SwitchChangeType.DEACTIVATED
+            assert northbound.getAllLinks().findAll { it.state == IslChangeType.FAILED }.size() == isls.size() * 2
+        }
+        isls.each { northbound.deleteLink(islUtils.toLinkParameters(it)) }
+        northbound.deleteSwitch(sw.dpId, false)
+
+        and: "Update the multi table field in kilda configuration"
+        northbound.updateKildaConfiguration(initConf)
+
+        and: "New switch connects"
+        lockKeeper.reviveSwitch(sw)
+        Wrappers.wait(discoveryInterval + WAIT_OFFSET) {
+            assert northbound.getSwitch(sw.dpId).state == SwitchChangeType.ACTIVATED
+            def allIsls = northbound.getAllLinks()
+            isls.each {
+                assert islUtils.getIslInfo(allIsls, it).get().state == IslChangeType.DISCOVERED
+                assert islUtils.getIslInfo(allIsls, it.reversed).get().state == IslChangeType.DISCOVERED
+            }
+        }
+
+        then: "Switch is added with switch property according to the kilda configuration"
+        northbound.getSwitchProperties(sw.dpId).multiTable == initConf.useMultiTable
     }
 }
